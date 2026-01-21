a2b2021deb6c30d51778d838912da6de8d39c32f
Rafael Matos <rafaelmatos.dev1@gmail.com>
2026-01-20 21:17:50 -0300

    fix: cors config

--- backend/barberflow/settings.py
+++ backend/barberflow/settings.py
@@ -62,6 +62,25 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
+# Permitir headers customizados e de autenticação
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+    'x-barbershop-slug',
+]
+CORS_EXPOSE_HEADERS = [
+    'Authorization',
+    'Content-Type',
+    'X-Barbershop-Slug',
+]
+
 # REST Framework settings
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': None,
